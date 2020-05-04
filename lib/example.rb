4740b8e6890aef6e78d1a65db7679cc183c86a11
Ghameerah McCullers <mccullers.ghameerah@gmail.com>
2020-05-04 17:46:59 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,14 +1,5 @@
 # Use this space to code along with the readme
 
 phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+puts "Hello World"
 end
